283f3e961757a81f4985504bbe34761e9de14381
cindy kivala <cindykivala@gmail.com>
2025-09-25 15:35:45 +0300

    run build

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function NavBar({ user, onLogout }) {
   return (
     <nav className="navbar">
-      {/* Brand / Logo */}
+      {/* Brand  Logo */}
       <Link to="/" className="navbar-brand brand-font">
         Cartify
       </Link>
